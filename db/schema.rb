a564ef7cbdd62be5c58dab39a205b26e0d247752
tomoyayoshino <t.yoshino0331@gmail.com>
2020-07-08 11:36:36 +0900

    ancestryの型をstringに変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_04_110512) do
+ActiveRecord::Schema.define(version: 2020_07_08_023432) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2020_07_04_110512) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.integer "ancestry", null: false
+    t.string "ancestry", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -73,7 +73,7 @@ ActiveRecord::Schema.define(version: 2020_07_04_110512) do
     t.string "last_name", null: false
     t.string "first_name_kana", null: false
     t.string "last_name_kana", null: false
-    t.date "birthday_year", null: false
+    t.date "birthday_year"
     t.date "birthday_month", null: false
     t.date "birthday_day", null: false
     t.string "tel"
