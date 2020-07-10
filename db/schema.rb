c186688e4d833d6ae50f1c27c7e824b793b1afff
tomoyayoshino <t.yoshino0331@gmail.com>
2020-07-10 14:30:35 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,18 @@
 
 ActiveRecord::Schema.define(version: 2020_07_08_025952) do
 
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "post_code", null: false
+    t.string "prefecture", null: false
+    t.string "city", null: false
+    t.integer "house_number", null: false
+    t.string "building_name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -86,6 +98,7 @@ ActiveRecord::Schema.define(version: 2020_07_08_025952) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "addresses", "users"
   add_foreign_key "products", "brands"
   add_foreign_key "products", "categories"
   add_foreign_key "products", "conditions"
