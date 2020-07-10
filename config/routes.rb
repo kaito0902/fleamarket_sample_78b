965776a20b10d5b11e75fec75f1c21292f74579f
tadashi666 <66359887+tadashi666@users.noreply.github.com>
2020-07-10 14:39:50 +0900

    商品購入前確認ページ（フロント実装）

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :products, only: [:index, :show, :new]
+  resources :buyers, only: :index do
+    collection do
+      get 'index', to: 'buyers#index'
+    end
+  end
 end
