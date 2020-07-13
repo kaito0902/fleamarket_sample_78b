9777da6ceb99755777d0ba773d8d5be582754198
Japanesecedarbook <yositakasugi1113@gmail.com>
2020-07-13 16:43:15 +0900

    コントローラーコメントアウト修正　マイグレーションファイル修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_13_015713) do
+ActiveRecord::Schema.define(version: 2020_07_13_073328) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -67,10 +67,10 @@ ActiveRecord::Schema.define(version: 2020_07_13_015713) do
     t.string "prefecture", null: false
     t.string "day", null: false
     t.integer "price", null: false
-    t.integer "saler_id"
-    t.integer "buyer_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "saler_id"
+    t.integer "buyer_id"
     t.index ["brand_id"], name: "index_products_on_brand_id"
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["condition_id"], name: "index_products_on_condition_id"
